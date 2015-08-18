eb4cf400a7ccc5b64cd677182a528f856b31bc63
sinanbora <sinanbora.cs@gmail.com>
2015-08-18 15:34:46 +0300

    alt kategori ana kategori modala eklendi

--- front-end/public/js/lib-js/urunler-js.js
+++ front-end/public/js/lib-js/urunler-js.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $('.modal-trigger').leanModal();
   //Ana kategori dropdown doldurma
   $("#btnAnaKategoriEkle").on("click", function(){
     var anaKategori = $("#inpAnaKategori").val()
