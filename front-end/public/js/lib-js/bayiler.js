7547d42f32100422d7b3dbe5b83f28de3fa6cb7e
Irfan Hasbek <irfanhasbek@Irfan-iMac.local>
2015-08-19 14:57:15 +0300

    satıs noktaları finish

--- front-end/public/js/lib-js/bayiler.js
+++ front-end/public/js/lib-js/bayiler.js
@@ -4,38 +4,34 @@ $(document).ready(function(){
             console.error(res.data);
             return;
         }
+        
         if($("#frmBayiler").attr("action")=="/bayiler/ekle"){
-            var tbody=$(".bayilerTable tbody");
-            var tableLength=tbody.find("tr").length +1;
-            var tr=$("<tr id="+res.data._id+"></tr>");
-            var tdSira=$("<td>"+tableLength+".</td>");
-            var tdBayiTuru=$("<td>"+res.data.bayiTuru+"</td>");
-            var tdIlAdi=$("<td>"+res.data.ilAdi+"</td>");
-            var tdilceAdi=$("<td>"+res.data.ilceAdi+"</td>");
-            var tdAd=$("<td>"+res.data.ad+"</td>");
-            var tdadres=$("<td>"+res.data.adres+"</td>");
-            var tdTel=$("<td>"+res.data.tel+"</td>");
-            var tdfax=$("<td>"+res.data.fax+"</td>");
-            var tdemail=$("<td>"+res.data.email+"</td>");
-            var tdweb=$("<td>"+res.data.web+"</td>");
-            var tdbuttons=$('<td id='+res.data._id+'><button class="btn btn-floating red tooltipped sil"  data-position="top" data-delay="50" data-tooltip="Sil"><i class="mdi-action-delete"></i></button><button class="btn btn-floating green tooltipped guncelle"  data-position="top" data-delay="50" data-tooltip="Değiştir"><i class="mdi-av-replay"></i></button></td>');
-            tbody.append(tr);
-            tr.append(tdSira);
-            tr.append(tdBayiTuru);
-            tr.append(tdIlAdi);
-            tr.append(tdilceAdi);
-            tr.append(tdAd);
-            tr.append(tdadres);
-            tr.append(tdTel);
-            tr.append(tdfax);
-            tr.append(tdemail);
-            tr.append(tdweb);
-            tr.append(tdbuttons);
+            addRow(res);
             allClear();
         
         }else{
+            
             $("#frmBayiler").attr("action","/bayiler/ekle");
             $("#btnEkle").text("ekle");
+            var id=$("#inpId").val();
+            wsPost("/bayiler/getir",{_id:id},function(err,result){
+                 if(err){
+                    console.error("error : "+err);
+                    return;
+                 }
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("1").text(result.data.bayiTuru);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("2").text(result.data.ilAdi);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("3").text(result.data.ilceAdi);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("4").text(result.data.ad);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("5").text(result.data.adres);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("6").text(result.data.tel);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("7").text(result.data.fax);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("8").text(result.data.email);
+                $(".bayilerTable tbody").find("tr[id="+$("#inpId").val()+"]").find("td").eq("9").text(result.data.web);
+                allClear();
+                alertify.success("Bilgileriniz başarı ile güncellendi.");
+            });
+            
         }
     });
     removeFromTable("bayilerTable","/bayiler/sil",function(id){
@@ -47,7 +43,7 @@ $(document).ready(function(){
         $("#btnEkle").text("Güncelle");
         var _id=$(this).closest("tr").attr("id");
         wsPost("/bayiler/getir",{_id:_id},function(err,result){
-            console.log(JSON.stringify(result.data));
+            
             if(err){
                 console.error("error : "+err);
                 return;
@@ -78,4 +74,33 @@ function allClear(){
     $("#divBayiler select").find('option:contains("Bayi \ Servisler Seç")').attr('selected', true);
     $("textarea").val("");
 
+}
+function addRow(res){
+    var tbody=$(".bayilerTable tbody");
+    var tableLength=tbody.find("tr").length +1;
+    var tr=$("<tr id="+res.data._id+"></tr>");
+    var tdSira=$("<td>"+tableLength+".</td>");
+    var tdBayiTuru=$("<td>"+res.data.bayiTuru+"</td>");
+    var tdIlAdi=$("<td>"+res.data.ilAdi+"</td>");
+    var tdilceAdi=$("<td>"+res.data.ilceAdi+"</td>");
+    var tdAd=$("<td>"+res.data.ad+"</td>");
+    var tdadres=$("<td>"+res.data.adres+"</td>");
+    var tdTel=$("<td>"+res.data.tel+"</td>");
+    var tdfax=$("<td>"+res.data.fax+"</td>");
+    var tdemail=$("<td>"+res.data.email+"</td>");
+    var tdweb=$("<td>"+res.data.web+"</td>");
+    var tdbuttons=$('<td id='+res.data._id+'><button class="btn btn-floating red tooltipped sil"  data-position="top" data-delay="50" data-tooltip="Sil"><i class="mdi-action-delete"></i></button><button class="btn btn-floating green tooltipped guncelle"  data-position="top" data-delay="50" data-tooltip="Değiştir"><i class="mdi-av-replay"></i></button></td>');
+    tbody.append(tr);
+    tr.append(tdSira);
+    tr.append(tdBayiTuru);
+    tr.append(tdIlAdi);
+    tr.append(tdilceAdi);
+    tr.append(tdAd);
+    tr.append(tdadres);
+    tr.append(tdTel);
+    tr.append(tdfax);
+    tr.append(tdemail);
+    tr.append(tdweb);
+    tr.append(tdbuttons);
+    alertify.success("bilgileriniz başarı ile eklendi.");
 }
