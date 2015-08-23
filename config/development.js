5298fb5ad1541d51d7735ad782ab4749eed45881
Irfan Hasbek <irfanhasbek@Irfan-iMac.local>
2015-08-23 19:56:24 +0300

    config

--- config/development.js
+++ config/development.js
@@ -2,6 +2,6 @@ module.exports = {
     dbpath : "mongodb://localhost/mkbandios",
     port : 3333,
     loglevel : 'debug',
-    host : "http://http://andios.mavikentbilisim.com/",
+    host : "http://andios.mavikentbilisim.com/",
     izinliUrlListesi : ["/", "/sayfalar/giris", "/hesap/giris", "/hesap/cikis", "/hesap/kaydol", "/sayfalar/kaydol", "/kurumsalizinler/ekle","/versiyon/kontrolet","/hakkimizda/getir","/urunler/getir","/bayiler/getir","/iletisim/getir","/hizmetler/getir","/haberler/getir","/etkinlikler/getir","/duyurular/getir","/uretim/getir","/isteksikayet/getir","/fiyatlistesi/getir","/ikpolitikasi/getir","/isstaj/getir","/aertifikalar/getir","/kurumsalvideo/getir","/kurumsalfotogaleri/getir"]
 };
