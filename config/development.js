487c9524252587475b445db31ebed3401093fb8c
Irfan Hasbek <irfanhasbek@Irfan-iMac.zyxel.com>
2015-08-24 14:37:44 +0300

    config 2

--- config/development.js
+++ config/development.js
@@ -3,5 +3,5 @@ module.exports = {
     port : 3333,
     loglevel : 'debug',
     host : "http://andios.mavikentbilisim.com/",
-    izinliUrlListesi : ["/", "/sayfalar/giris", "/hesap/giris", "/hesap/cikis", "/hesap/kaydol", "/sayfalar/kaydol", "/kurumsalizinler/ekle","/versiyon/kontrolet","/hakkimizda/getir","/urunler/getir","/bayiler/getir","/iletisim/getir","/hizmetler/getir","/haberler/getir","/etkinlikler/getir","/duyurular/getir","/uretim/getir","/isteksikayet/getir","/fiyatlistesi/getir","/ikpolitikasi/getir","/isstaj/getir","/aertifikalar/getir","/kurumsalvideo/getir","/kurumsalfotogaleri/getir"]
+    izinliUrlListesi : ["/", "/sayfalar/giris", "/hesap/giris", "/hesap/cikis", "/hesap/kaydol", "/sayfalar/kaydol", "/kurumsalizinler/ekle","/versiyon/kontrolet","/hakkimizda/getir","/urunler/getir","/bayiler/getir","/iletisim/getir","/hizmetler/getir","/haberler/getir","/etkinlikler/getir","/duyurular/getir","/uretim/getir","/isteksikayet/getir","/fiyatlistesi/getir","/ikpolitikasi/getir","/isstaj/getir","/aertifikalar/getir","/kurumsalvideo/getir","/kurumsalfotogaleri/getir","/urunler/ara"]
 };
