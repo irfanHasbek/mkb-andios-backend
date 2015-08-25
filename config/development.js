279355650738e08982d9776f47f0c19719ed6a70
irfan Hasbek <irfanhasbek@gmail.com>
2015-08-25 18:03:30 +0300

    config guncellendi

--- config/development.js
+++ config/development.js
@@ -3,5 +3,5 @@ module.exports = {
     port : 3333,
     loglevel : 'debug',
     host : "http://andios.mavikentbilisim.com/",
-    izinliUrlListesi : ["/", "/sayfalar/giris", "/hesap/giris", "/hesap/cikis", "/hesap/kaydol", "/sayfalar/kaydol", "/kurumsalizinler/ekle","/versiyon/kontrolet","/hakkimizda/getir","/urunler/getir","/bayiler/getir","/iletisim/getir","/hizmetler/getir","/haberler/getir","/etkinlikler/getir","/duyurular/getir","/uretim/getir","/isteksikayet/getir","/fiyatlistesi/getir","/ikpolitikasi/getir","/isstaj/getir","/sertifikalar/ara","/kurumsalvideo/ara","/kurumsalfotogaleri/ara","/urunler/ara"]
+    izinliUrlListesi : ["/", "/sayfalar/giris", "/hesap/giris", "/hesap/cikis", "/hesap/kaydol", "/sayfalar/kaydol", "/kurumsalizinler/ekle","/versiyon/kontrolet","/hakkimizda/getir","/urunler/getir","/bayiler/getir","/iletisim/getir","/hizmetler/ara","/haberler/ara","/etkinlikler/ara","/duyurular/ara","/uretim/ara","/isteksikayet/ara","/fiyatlistesi/ara","/ikpolitikasi/getir","/isstaj/ara","/sertifikalar/ara","/kurumsalvideo/ara","/kurumsalfotogaleri/ara","/urunler/ara"]
 };
