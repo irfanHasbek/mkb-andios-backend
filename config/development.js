add5a5d5da1758621f43b45bb44401fed2af732c
irfan Hasbek <irfanhasbek@gmail.com>
2015-08-25 14:29:41 +0300

    config degisti

--- config/development.js
+++ config/development.js
@@ -2,6 +2,6 @@ module.exports = {
     dbpath : "mongodb://localhost/mkbandios",
     port : 3333,
     loglevel : 'debug',
-    host : "http://localhost:3333/",
-    izinliUrlListesi : ["/", "/sayfalar/giris", "/hesap/giris", "/hesap/cikis", "/hesap/kaydol", "/sayfalar/kaydol", "/kurumsalizinler/ekle","/versiyon/kontrolet","/hakkimizda/getir","/urunler/getir","/bayiler/getir","/iletisim/getir","/hizmetler/getir","/haberler/getir","/etkinlikler/getir","/duyurular/getir","/uretim/getir","/isteksikayet/getir","/fiyatlistesi/getir","/ikpolitikasi/getir","/isstaj/getir","/aertifikalar/getir","/kurumsalvideo/getir","/kurumsalfotogaleri/getir","/urunler/ara"]
+    host : "http://andios.mavikentbilisim.com/",
+    izinliUrlListesi : ["/", "/sayfalar/giris", "/hesap/giris", "/hesap/cikis", "/hesap/kaydol", "/sayfalar/kaydol", "/kurumsalizinler/ekle","/versiyon/kontrolet","/hakkimizda/getir","/urunler/getir","/bayiler/getir","/iletisim/getir","/hizmetler/getir","/haberler/getir","/etkinlikler/getir","/duyurular/getir","/uretim/getir","/isteksikayet/getir","/fiyatlistesi/getir","/ikpolitikasi/getir","/isstaj/getir","/sertifikalar/ara","/kurumsalvideo/ara","/kurumsalfotogaleri/ara","/urunler/ara"]
 };
