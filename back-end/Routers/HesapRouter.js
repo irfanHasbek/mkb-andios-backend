36a7f992b42c77db09c41dc9a715616cb3e45775
irfan Hasbek <irfanhasbek@gmail.com>
2015-08-27 13:48:33 +0300

    versiyon guncellendi

--- back-end/Routers/HesapRouter.js
+++ back-end/Routers/HesapRouter.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs')
 var KullaniciModeli = require('../Modeller/KullaniciModeli');
+var VersionModeli = require('../Modeller/VersiyonModeli')
 
 function HesapRouter(){
     var router = express.Router();
@@ -34,31 +35,49 @@ function HesapRouter(){
         var kullaniciBilgileri = req.body;
         var yeniKullanici = new KullaniciModeli(kullaniciBilgileri);
         yeniKullanici.save(function(kullaniciHata, kullanici){
+          console.log(kullanici);
           if (kullanici) {
-            req.session.kullanici = kullanici;
-            req.session.giris = true;
-            req.session.mesaj = "Giris Basarili !";
-            var dir = './front-end/public/yuklemeler/' + kullanici._id;
-            if (!fs.existsSync(dir)){
-                fs.mkdirSync(dir);
-                var dirDosya = './front-end/public/yuklemeler/' + kullanici._id + "/dosyalar";
-                var dirMedya = './front-end/public/yuklemeler/' + kullanici._id + "/medyalar";
-                if (!fs.existsSync(dirDosya) && !fs.existsSync(dirMedya)) {
-                  fs.mkdirSync(dirDosya);
-                  fs.mkdirSync(dirMedya);
+            var yeniVersiyon = new VersionModeli({mobilVersiyon : "0", kullaniciKodu : kullanici.kullaniciKodu});
+            yeniVersiyon.save(function (versiyonHata, versiyon) {
+              if (versiyonHata) {
+                hataVer(req, res)
+              }
+              if (versiyon) {
+                req.session.kullanici = kullanici;
+                req.session.giris = true;
+                req.session.mesaj = "Giris Basarili !";
+                var dir = './front-end/public/yuklemeler/' + kullanici._id;
+                if (!fs.existsSync(dir)){
+                    fs.mkdirSync(dir);
+                    var dirDosya = './front-end/public/yuklemeler/' + kullanici._id + "/dosyalar";
+                    var dirMedya = './front-end/public/yuklemeler/' + kullanici._id + "/medyalar";
+                    if (!fs.existsSync(dirDosya) && !fs.existsSync(dirMedya)) {
+                      fs.mkdirSync(dirDosya);
+                      fs.mkdirSync(dirMedya);
+                    }
+                    res.redirect('/sayfalar/anasayfa');
+                }else {
+                  hataVer(req, res)
                 }
-            }
-            res.redirect('/sayfalar/anasayfa');
-          }else {
-            req.session.kullanici = null;
-            req.session.giris = false;
-            req.session.mesaj = "Giris Basarisiz ! Lütfen bilgilerinizi kontrol edin. !";
+              }else {
+                hataVer(req, res)
+              }
+            })
 
-            res.redirect('/sayfalar/giris');
+          }else {
+            hataVer(req, res, kullaniciHata)
           }
         });
     });
 
     return router;
+}
+
+function hataVer (req, res, hata) {
+  req.session.kullanici = null;
+  req.session.giris = false;
+  req.session.mesaj = "Giris Basarisiz ! Lütfen servis saglayicinizla iletisime gecin !";
+  console.log("Hata : " + hata);
+  res.redirect('/sayfalar/giris');
 }
 module.exports = HesapRouter;
--- back-end/Routers/VersiyonRouter.js
+++ back-end/Routers/VersiyonRouter.js
@@ -6,7 +6,10 @@ function Versiyon(model){
     router.post('/kontrolet', function(req, res) {
         var kullaniciKodu = req.body.kullaniciKodu;
         var mobilVersiyon = req.body.versiyon;
+        console.log("kullaniciKodu : " + kullaniciKodu);
+        console.log("mobilVersiyon : " + mobilVersiyon);
         VersionModeli.findOne({kullaniciKodu : kullaniciKodu}, function(dbHatasi, listelenen) {
+            console.log("Kullanici Versiyon : " + listelenen);
             if(dbHatasi) {
                 res.send({state : false, data : dbHatasi});
                 return;
@@ -14,10 +17,12 @@ function Versiyon(model){
             else{
                 if (listelenen) {
                   if (listelenen.mobilVersiyon != mobilVersiyon) {
-                    res.send({state : true, data : { bilgi : listelenen, versiyon : false}});
+                    res.send({state : true, data : { bilgi : listelenen, guncellemeVar : true}});
                     return;
                   }
-                  res.send({state : true, data : { bilgi : listelenen, versiyon : true}});
+                  res.send({state : true, data : { bilgi : listelenen, guncellemeVar : false}});
+                }else {
+                  res.send({state : false, data : { bilgi : "Kullanici bulunamadi !", guncellemeVar : false}});
                 }
 
             }
